0695ed0dd02491a2c4ab6888242fb9e0e64e27f9
chintanrparmar <chintanrparmar@gmail.com>
2020-06-03 20:14:03 +0530

    Pav Bhaji Recipe Added.

--- app/src/main/java/it/cook/model/Recipe.kt
+++ app/src/main/java/it/cook/model/Recipe.kt
@@ -4,6 +4,7 @@ data class Recipe(
     val cookingTime: String,
     val cover: String,
     val prepTime: String,
+    val category: String?,
     val ingredients: String?,
     val stepsGuide: String?,
     val id: Int,
